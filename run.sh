bae4a81002618df223959e246595c0b7d5c1e7c6
kristupas <kristupas@pcstransfer.reputeme.com>
2022-04-27 07:41:47 +0000

    Added jmeter dependancy, created 'tests/' #13

--- run.sh
+++ run.sh
@@ -42,6 +42,29 @@ install_plpython() {
     sudo apt-get -y install python3 postgresql-plpython3-12
 }
 
+install_jmeter() {
+    sudo apt-get update -y
+    sudo apt-get install openjdk-8-jdk -y
+
+    wget "https://downloads.apache.org//jmeter/binaries/apache-jmeter-${jmeter_v}.zip"
+
+    jmeter_src="apache-jmeter-${jmeter_v}"
+    sudo unzip "${jmeter_src}.zip" -d '/usr/local/src'
+    rm "${jmeter_src}.zip"
+
+    jmeter="jmeter-${jmeter_v}"
+    touch $jmeter
+    echo '#!/usr/bin/env bash' >> $jmeter
+    echo '' >> $jmeter
+    echo "cd '/usr/local/src/apache-jmeter-${jmeter_v}/bin'" >> $jmeter
+    echo './jmeter $@' >> $jmeter
+    sudo chmod +x $jmeter
+    sudo mv $jmeter "/usr/local/bin/${jmeter}"
+}
+
+postgrest_v=9.0.0
+jmeter_v=5.4.3
+
 if [ "$1" = "start" ]; then
     start_webserver $2
 elif [ "$1" = "re-start" ]; then
@@ -53,6 +76,8 @@ elif [ "$1" =  "install-postgrest" ]; then
     install_postgrest
 elif [ "$1" =  "install-plpython" ]; then
     install_plpython
+elif [ "$1" =  "install-jmeter" ]; then
+    install_jmeter
 else
     echo "job not found"
     exit 1
