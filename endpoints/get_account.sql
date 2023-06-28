cc4f9d525a72bd2583ef6bd9368b77acd90e3cb2
mzander <jacekz58@wp.pl>
2023-06-28 12:40:32 +0200

    Change get_account so it returns btracker balances

--- endpoints/get_account.sql
+++ endpoints/get_account.sql
@@ -265,16 +265,50 @@ BEGIN
     SELECT NULL INTO __posting_json_metadata;
   END;
 
+  WITH btracker_balance AS 
+  (
+  SELECT
+  COALESCE(_result_balance.hbd_balance, 0)AS hbd_balance,
+  COALESCE(_result_balance.hive_balance, 0) AS hive_balance,
+  COALESCE(_result_balance.vesting_shares, 0) AS vesting_shares,
+  COALESCE(_result_balance.vesting_balance_hive, 0) AS vesting_balance_hive,
+  COALESCE(_result_balance.post_voting_power_vests, 0) AS post_voting_power_vests,
+  COALESCE(_result_withdraws.vesting_withdraw_rate, 0) AS vesting_withdraw_rate,
+  COALESCE(_result_withdraws.to_withdraw, 0) AS to_withdraw,
+  COALESCE(_result_withdraws.withdrawn, 0) AS withdrawn,
+  COALESCE(_result_withdraws.withdraw_routes, 0) AS withdraw_routes,
+  COALESCE(_result_vest_balance.delegated_vests, 0) AS delegated_vests,
+  COALESCE(_result_vest_balance.received_vests, 0) AS received_vests,
+  _result_json_metadata.json_metadata AS json_metadata,
+  _result_json_metadata.posting_json_metadata AS posting_json_metadata,
+  COALESCE(_result_rewards.hbd_rewards, 0) AS hbd_rewards,
+  COALESCE(_result_rewards.hive_rewards, 0) AS hive_rewards,
+  COALESCE(_result_rewards.vests_rewards, 0) AS vests_rewards,
+  COALESCE(_result_rewards.hive_vesting_rewards, 0) AS hive_vesting_rewards,
+  COALESCE(_result_savings.hbd_savings, 0) AS hbd_savings,
+  COALESCE(_result_savings.hive_savings, 0) AS hive_savings,
+  COALESCE(_result_savings.savings_withdraw_requests, 0) AS savings_withdraw_requests,
+  _id.id
+  FROM
+  (SELECT * FROM hafbe_endpoints.get_btracker_account_balance('abit')) AS _result_balance,
+  (SELECT * FROM hafbe_endpoints.get_current_account_withdraws('abit')) AS _result_withdraws,
+  (SELECT * FROM hafbe_endpoints.get_btracker_vests_balance('abit')) AS _result_vest_balance,
+  (SELECT * FROM hafbe_endpoints.get_json_metadata('abit')) AS _result_json_metadata,
+  (SELECT * FROM hafbe_endpoints.get_current_account_rewards('abit')) AS _result_rewards,
+  (SELECT * FROM hafbe_endpoints.get_current_account_savings('abit')) AS _result_savings,
+  (SELECT id FROM hive.accounts_view where name = 'abit') AS _id
+  )
+
   SELECT json_build_object(
-    'id', __response_data->>'id', --OK
+    'id', id, --OK
     'name', _account, --OK
     'owner', __response_data->'owner', --work in progress
     'active', __response_data->'active', --work in progress
     'posting', __response_data->'posting', --work in progress
     'memo_key', __response_data->>'memo_key', --??
     'profile_image', __profile_image, --OK
-    'json_metadata', __json_metadata, --OK
-    'posting_json_metadata', __posting_json_metadata, --OK
+    'json_metadata', json_metadata, --OK
+    'posting_json_metadata', posting_json_metadata, --OK
     'last_owner_update', __response_data->>'last_owner_update', 
     'last_account_update', __response_data->>'last_account_update', 
     'created', __response_data->>'created',
@@ -286,30 +320,34 @@ BEGIN
     'voting_manabar', __response_data->'voting_manabar',
     'downvote_manabar', __response_data->'downvote_manabar',
     'voting_power', __response_data->>'voting_power',
-    'balance', __response_data->>'balance', --OK
-    'savings_balance', __response_data->>'savings_balance', --OK
-    'hbd_balance', __response_data->>'hbd_balance', --OK
-    --hbd_saving_balance OK
-    'savings_withdraw_requests', __response_data->>'savings_withdraw_requests', --OK
-    'reward_hbd_balance', __response_data->>'reward_hbd_balance', --OK
-    'reward_hive_balance', __response_data->>'reward_hive_balance', --OK
-    'reward_vesting_balance', __response_data->>'reward_vesting_balance', --OK
-    'reward_vesting_hive', __response_data->>'reward_vesting_hive', --OK
-    'vesting_shares', __response_data->>'vesting_shares', --OK 
-    'delegated_vesting_shares', __response_data->>'delegated_vesting_shares', --OK
-    'received_vesting_shares', __response_data->>'received_vesting_shares', --OK
-    'vesting_withdraw_rate', __response_data->>'vesting_withdraw_rate',
-    'post_voting_power', __response_data->>'post_voting_power',
-    'posting_rewards', __response_data->>'posting_rewards',
+    'balance', hive_balance, --OK
+    'savings_balance', hive_savings, --OK
+    'hbd_balance', hbd_balance, --OK
+    'hbd_saving_balance', hbd_savings, --OK
+    'savings_withdraw_requests', savings_withdraw_requests, --OK
+    'reward_hbd_balance', hbd_rewards, --OK
+    'reward_hive_balance', hive_rewards, --OK
+    'reward_vesting_balance', vests_rewards, --OK
+    'reward_vesting_hive', hive_vesting_rewards, --OK
+    'vesting_shares', vesting_shares, --OK 
+    'delegated_vesting_shares', delegated_vests, --OK
+    'received_vesting_shares', received_vests, --OK
+    'vesting_withdraw_rate', vesting_withdraw_rate, --OK
+    'to_withdraw', to_withdraw,--OK
+    'withdrawn', withdrawn,--OK
+    'withdraw_routes', withdraw_routes,--OK
+    'post_voting_power', post_voting_power_vests, --OK
+    'posting_rewards', __response_data->>'posting_rewards', --do we want it? curation_rewards aswell
     'proxied_vsf_votes', __response_data->'proxied_vsf_votes',
     'witnesses_voted_for', __response_data->>'witnesses_voted_for',
     'last_post', __response_data->>'last_post',
     'last_root_post', __response_data->>'last_root_post',
     'last_vote_time', __response_data->>'last_vote_time',
-    'vesting_balance', __response_data->>'vesting_balance', --OK, NOT USED ANYMORE (ALWAYS 0)
+    'vesting_balance', vesting_balance_hive, --OK
     'reputation', __response_data->>'reputation', --work in progress
     'witness_votes', __response_data->'witness_votes'
-  ) INTO __response_data;
+  ) INTO __response_data
+   FROM btracker_balance;
 
   INSERT INTO hafbe_app.hived_account_cache (account, data, last_updated_at)
   SELECT _account, __response_data, NOW()
