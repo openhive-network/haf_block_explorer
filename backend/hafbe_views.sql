00e56043211d8f8a0409999ed44133083a7da948
mzander <jacekz58@wp.pl>
2023-08-29 12:47:58 +0200

    Update views so it uses new ways of gathering data (delayed votes and balance from btracker)

--- backend/hafbe_views.sql
+++ backend/hafbe_views.sql
@@ -20,69 +20,102 @@ CREATE OR REPLACE VIEW hafbe_views.recursive_account_proxies_view AS
 WITH proxies1 AS (
   SELECT
     prox1.proxy_id AS top_proxy_id,
-    prox1.account_id
+    prox1.account_id, 1 AS which_proxy
   FROM hafbe_app.current_account_proxies prox1
 ),
 
 proxies2 AS (
-  SELECT prox1.top_proxy_id, prox2.account_id
+  SELECT prox1.top_proxy_id, prox2.account_id, 2 AS which_proxy
   FROM proxies1 prox1
   JOIN hafbe_app.current_account_proxies prox2 ON prox2.proxy_id = prox1.account_id
 ),
 
 proxies3 AS (
-  SELECT prox2.top_proxy_id, prox3.account_id
+  SELECT prox2.top_proxy_id, prox3.account_id, 3 AS which_proxy
   FROM proxies2 prox2
   JOIN hafbe_app.current_account_proxies prox3 ON prox3.proxy_id = prox2.account_id
 ),
 
 proxies4 AS (
-  SELECT prox3.top_proxy_id, prox4.account_id
+  SELECT prox3.top_proxy_id, prox4.account_id, 4 AS which_proxy
   FROM proxies3 prox3
   JOIN hafbe_app.current_account_proxies prox4 ON prox4.proxy_id = prox3.account_id
 ),
-
 proxies5 AS (
-  SELECT prox4.top_proxy_id, prox5.account_id
+  SELECT prox4.top_proxy_id, prox5.account_id, 5 AS which_proxy
   FROM proxies4 prox4
   JOIN hafbe_app.current_account_proxies prox5 ON prox5.proxy_id = prox4.account_id
 )
 
-SELECT top_proxy_id AS proxy_id, account_id
+SELECT top_proxy_id AS proxy_id, account_id, which_proxy
 FROM (
-  SELECT top_proxy_id, account_id FROM proxies1
+  SELECT top_proxy_id, account_id, which_proxy FROM proxies1
   UNION
-  SELECT top_proxy_id, account_id FROM proxies2
+  SELECT top_proxy_id, account_id, which_proxy FROM proxies2
   UNION
-  SELECT top_proxy_id, account_id FROM proxies3
+  SELECT top_proxy_id, account_id, which_proxy FROM proxies3
   UNION
-  SELECT top_proxy_id, account_id FROM proxies4
+  SELECT top_proxy_id, account_id, which_proxy FROM proxies4
   UNION
-  SELECT top_proxy_id, account_id FROM proxies5
+  SELECT top_proxy_id, account_id, which_proxy FROM proxies5
 ) rap;
+------
+
+CREATE OR REPLACE VIEW hafbe_views.recursive_account_proxies_stats_view AS
+SELECT
+  rapv.proxy_id,
+  avv.name,
+  (av.balance - COALESCE(dv.delayed_vests,0)) AS proxied_vests,
+  rapv.which_proxy
+FROM hafbe_views.recursive_account_proxies_view rapv
+JOIN btracker_app.current_account_balances av ON av.account = rapv.account_id AND av.nai = 37
+LEFT JOIN btracker_app.account_withdraws dv ON dv.account = rapv.account_id
+JOIN hive.accounts_view avv ON avv.id = rapv.account_id;
 
 ------
 
 CREATE OR REPLACE VIEW hafbe_views.witness_voters_vests_view AS
 SELECT
   cwv_cap.witness_id, cwv_cap.voter_id,
-  CASE WHEN cwv_cap.proxy_id IS NULL THEN COALESCE(av.vests, 0) ELSE 0 END AS account_vests,
+  CASE WHEN cwv_cap.proxy_id IS NULL THEN COALESCE(av.balance, 0) ELSE 0 END AS account_vests,
   cwv_cap.timestamp
 FROM (
   SELECT cwv.witness_id, cwv.voter_id, cwv.timestamp, cap.proxy_id
   FROM hafbe_app.current_witness_votes cwv
   LEFT JOIN hafbe_app.current_account_proxies cap ON cap.account_id = cwv.voter_id
 ) cwv_cap
-LEFT JOIN hafbe_app.account_vests av ON av.account_id = cwv_cap.voter_id;
+LEFT JOIN btracker_app.current_account_balances av ON av.account = cwv_cap.voter_id AND av.nai = 37;
+
+------
+
+CREATE OR REPLACE VIEW hafbe_views.current_witness_votes_view AS
+  SELECT
+    ov.voter_id AS account,
+    vv.name AS vote
+  FROM
+    hafbe_app.current_witness_votes ov
+  JOIN 
+    hive.accounts_view vv ON vv.id = ov.witness_id;
 
 ------
 
 CREATE OR REPLACE VIEW hafbe_views.voters_proxied_vests_view AS
 SELECT
   rapv.proxy_id,
-  SUM(av.vests) AS proxied_vests
+  SUM(av.balance - COALESCE(dv.delayed_vests,0))::BIGINT AS proxied_vests,
+  rapv.which_proxy
 FROM hafbe_views.recursive_account_proxies_view rapv
-JOIN hafbe_app.account_vests av ON av.account_id = rapv.account_id
+JOIN btracker_app.current_account_balances av ON av.account = rapv.account_id AND av.nai = 37
+LEFT JOIN btracker_app.account_withdraws dv ON dv.account = rapv.account_id
+GROUP BY rapv.proxy_id, rapv.which_proxy;
+
+------
+
+CREATE OR REPLACE VIEW hafbe_views.voters_proxied_vests_sum_view AS
+SELECT
+  rapv.proxy_id,
+  SUM(rapv.proxied_vests) AS proxied_vests
+FROM hafbe_views.voters_proxied_vests_view rapv
 GROUP BY rapv.proxy_id;
 
 ------
@@ -90,30 +123,31 @@ GROUP BY rapv.proxy_id;
 CREATE OR REPLACE VIEW hafbe_views.voters_stats_view AS
 SELECT
   wvvv.witness_id, wvvv.voter_id,
-  wvvv.account_vests + COALESCE(vpvv.proxied_vests, 0) AS vests,
-  wvvv.account_vests,
+  wvvv.account_vests - COALESCE(dv.delayed_vests,0) + COALESCE(vpvv.proxied_vests, 0) AS vests,
+  wvvv.account_vests AS account_vests,
   COALESCE(vpvv.proxied_vests, 0) AS proxied_vests,
   wvvv.timestamp
 FROM hafbe_views.witness_voters_vests_view wvvv
-LEFT JOIN hafbe_views.voters_proxied_vests_view vpvv ON vpvv.proxy_id = wvvv.voter_id;
+LEFT JOIN hafbe_views.voters_proxied_vests_sum_view vpvv ON vpvv.proxy_id = wvvv.voter_id
+LEFT JOIN btracker_app.account_withdraws dv ON dv.account = wvvv.voter_id;
 
 ------
 
 CREATE OR REPLACE VIEW hafbe_views.voters_approve_vests_change_view AS
 SELECT
   wvh.witness_id, wvh.voter_id, wvh.approve, wvh.timestamp,
-  CASE WHEN wvh.approve THEN av.vests ELSE -1 * av.vests END AS account_vests,
+  CASE WHEN wvh.approve THEN av.balance ELSE -1 * av.balance END AS account_vests,
   CASE WHEN wvh.approve THEN COALESCE(rpav.proxied_vests, 0) ELSE -1 * COALESCE(rpav.proxied_vests, 0) END AS proxied_vests
 FROM hafbe_app.witness_votes_history wvh
 
 JOIN (
-  SELECT vests, account_id
-  FROM hafbe_app.account_vests
-) av ON av.account_id = wvh.voter_id
+  SELECT balance, account, nai
+  FROM btracker_app.current_account_balances
+) av ON av.account = wvh.voter_id AND av.nai = 37
 
 LEFT JOIN LATERAL (
   SELECT proxy_id, proxied_vests
-  FROM hafbe_views.voters_proxied_vests_view
+  FROM hafbe_views.voters_proxied_vests_sum_view
   WHERE proxy_id = wvh.voter_id
 ) rpav ON TRUE;
 
@@ -122,18 +156,18 @@ LEFT JOIN LATERAL (
 CREATE OR REPLACE VIEW hafbe_views.voters_proxy_vests_change_view AS
 SELECT
   aph.account_id AS voter_id,
-  SUM(CASE WHEN aph.proxy THEN -1 * av.vests ELSE av.vests END) AS account_vests,
+  SUM(CASE WHEN aph.proxy THEN -1 * av.balance ELSE av.balance END) AS account_vests,
   SUM(CASE WHEN aph.proxy THEN -1 * COALESCE(rpav.proxied_vests, 0) ELSE COALESCE(rpav.proxied_vests, 0) END) AS proxied_vests
 FROM hafbe_app.account_proxies_history aph
 
 JOIN (
-  SELECT vests, account_id
-  FROM hafbe_app.account_vests
-) av ON av.account_id = aph.account_id
+  SELECT balance, account, nai
+  FROM btracker_app.current_account_balances 
+) av ON av.account = aph.account_id AND av.nai = 37
 
 LEFT JOIN LATERAL (
   SELECT proxy_id, proxied_vests
-  FROM hafbe_views.voters_proxied_vests_view
+  FROM hafbe_views.voters_proxied_vests_sum_view
   WHERE proxy_id = aph.account_id
 ) rpav ON TRUE
 GROUP BY aph.account_id;
