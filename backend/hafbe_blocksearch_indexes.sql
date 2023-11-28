b855403fe829d397fc284ffa74534143f8cccd15
Michal Zander <mzander@syncad.com>
2023-11-28 15:30:08 +0000

    Fix block_search indexes and add comment search indexes

--- backend/hafbe_blocksearch_indexes.sql
+++ backend/hafbe_blocksearch_indexes.sql
@@ -1,74 +1,89 @@
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE EXTENSION IF NOT EXISTS btree_gin;
 
 CREATE INDEX IF NOT EXISTS hive_operations_comment_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_permlink')
 )
 WHERE op_type_id = 1;
 
 CREATE INDEX IF NOT EXISTS hive_operations_vote_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'voter') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'weight') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
-WHERE op_type_id = any(ARRAY[0, 72]);
+WHERE op_type_id = 0;
 
 CREATE INDEX IF NOT EXISTS hive_operations_delete_comment_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = any(ARRAY[17, 73]);
 
 
 CREATE INDEX IF NOT EXISTS hive_operations_comment_options_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = 19;
 
 CREATE INDEX IF NOT EXISTS hive_operations_author_reward_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = 51;
 
 CREATE INDEX IF NOT EXISTS hive_operations_comment_benefactor_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = 63;
 
 CREATE INDEX IF NOT EXISTS hive_operations_comment_payout_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = 61;
 
 CREATE INDEX IF NOT EXISTS hive_operations_comment_reward_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE op_type_id = 53;
 
-/*
-
-CREATE INDEX IF NOT EXISTS hive_operations_author_permlink ON hive.operations USING gin
+CREATE INDEX IF NOT EXISTS hive_operations_effective_vote_author_permlink ON hive.operations USING gin
 (
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author') gin_trgm_ops,
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink') gin_trgm_ops
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
+WHERE op_type_id = 72;
 
+------------------------------------------
+
+
+CREATE INDEX IF NOT EXISTS hive_operations_comment_search_permlink_author ON hive.operations USING gin
+(
+    (body_binary::jsonb -> 'value' ->> 'author'),
+    (body_binary::jsonb -> 'value' ->> 'permlink')
+)
+WHERE op_type_id IN (0, 1, 17, 19, 51, 53, 61, 63, 72, 73);
+
+CREATE INDEX IF NOT EXISTS hive_operations_comment_search_curation_author_permlink ON hive.operations USING gin
+(
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'comment_author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'comment_permlink')
+)
+WHERE op_type_id = 52;
+
+/*
 
 0	"hive::protocol::vote_operation"
 1	"hive::protocol::comment_operation"
@@ -80,9 +95,6 @@ CREATE INDEX IF NOT EXISTS hive_operations_author_permlink ON hive.operations US
 63	"hive::protocol::comment_benefactor_reward_operation"
 72	"hive::protocol::effective_comment_vote_operation"
 73	"hive::protocol::ineffective_delete_comment_operation"
-
-
-WHERE op_type_id + 1 IN (1,2,18,20,52,54,62,64,73,74);
 
 */
 
