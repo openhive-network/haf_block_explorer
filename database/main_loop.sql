9d3970ac0d8789fcff8a8dc0eb8987e4301ad630
Dan Notestein <dan@syncad.com>
2023-11-29 22:54:37 +0000

    async commits during massive sync

--- database/main_loop.sql
+++ database/main_loop.sql
@@ -14,13 +14,17 @@ DECLARE
   __last_block INT;
   __next_block_range hive.blocks_range;
   __block_range_len INT := 0;
-   __massive_processing_threshold INT := 100;
+  __massive_processing_threshold INT := 100;
+  __original_commit_mode TEXT;
+  __commit_mode_changed BOOLEAN := false;
+
 BEGIN
   PERFORM hafbe_app.allowProcessing();
   COMMIT;
 
-  SELECT hafbe_app.lastProcessedBlock() INTO __last_block;
+  SELECT current_setting('synchronous_commit') into __original_commit_mode;
 
+  SELECT hafbe_app.lastProcessedBlock() INTO __last_block;
   RAISE NOTICE 'Last block processed by application: %', __last_block;
 
   IF NOT hive.app_context_are_attached(ARRAY[_appContext, _appContext_btracker]) THEN
@@ -55,8 +59,16 @@ BEGIN
       __block_range_len := __next_block_range.last_block - __next_block_range.first_block + 1;
 
       IF __block_range_len >= __massive_processing_threshold THEN
+        IF NOT __commit_mode_changed AND __original_commit_mode != 'OFF' THEN
+          PERFORM set_config('synchronous_commit', 'OFF', false);
+          __commit_mode_changed := true;
+        END IF;
         CALL hafbe_app.do_massive_processing(_appContext, _appContext_btracker, __next_block_range.first_block, __next_block_range.last_block, 10000, __last_block);
       ELSE
+        IF __commit_mode_changed THEN
+          PERFORM set_config('synchronous_commit', __original_commit_mode, false);
+          __commit_mode_changed := false;
+        END IF;
         FOR __block IN __next_block_range.first_block .. __next_block_range.last_block LOOP
           CALL hafbe_app.processBlock(__block, _appContext);
           __last_block := __block;
@@ -72,8 +84,8 @@ BEGIN
       END IF;
 
       IF __next_block_range.first_block = __next_block_range.last_block AND 
-      (SELECT finished_processing_at FROM hafbe_app.app_status LIMIT 1) IS NULL THEN
-      UPDATE hafbe_app.app_status SET finished_processing_at = NOW();
+         (SELECT finished_processing_at FROM hafbe_app.app_status LIMIT 1) IS NULL THEN
+        UPDATE hafbe_app.app_status SET finished_processing_at = NOW();
         PERFORM hafbe_indexes.create_hafbe_indexes();
         PERFORM hafbe_indexes.create_btracker_indexes();
         -- we used to analyze the entire database here, but I'm not sure there's any reason to do so.
