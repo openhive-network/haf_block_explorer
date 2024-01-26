a37631611bc086946945fac7f5635d1720477a13
Dan Notestein <dan@syncad.com>
2024-01-26 04:39:32 +0000

    If hafbe_owner exists don't assume hafbe_user does

--- database/builtin_roles.sql
+++ database/builtin_roles.sql
@@ -2,9 +2,13 @@
 DO $$
 BEGIN
   CREATE ROLE hafbe_owner WITH LOGIN INHERIT IN ROLE hive_applications_group;
+EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
+END
+$$;
 
-  CREATE ROLE hafbe_user WITH LOGIN INHERIT IN ROLE hive_applications_group;
-  
+DO $$
+BEGIN
+  CREATE ROLE hafbe_user WITH LOGIN INHERIT IN ROLE hive_applications_group;  
 EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
 END
 $$;
