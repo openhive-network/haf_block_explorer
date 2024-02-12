70c88771cd30f03f6f4040ee9480635516d2a5ae
Michal Zander <mzander@syncad.com>
2024-02-12 12:29:25 +0000

    Add enpoint for live view of account operations

--- endpoints/get_operations.sql
+++ endpoints/get_operations.sql
@@ -256,4 +256,64 @@ END
 $$;
 
 
+CREATE OR REPLACE FUNCTION hafbe_endpoints.get_last_ops_by_account(
+    _account TEXT,
+    _filter INT [] = NULL,
+    _limit INT = 20,
+    _body_limit INT = 2147483647
+)
+RETURNS SETOF hafbe_types.operation -- noqa: LT01, CP05
+LANGUAGE 'plpgsql' STABLE
+COST 10000
+SET JIT = OFF
+SET join_collapse_limit = 16
+SET from_collapse_limit = 16
+AS
+$$
+DECLARE
+  __no_ops_filter BOOLEAN = (_filter IS NULL);
+  __account_id INT := (SELECT av.id FROM hive.accounts_view av WHERE av.name = _account);
+BEGIN
+
+RETURN QUERY   
+WITH operation_range AS MATERIALIZED (
+  SELECT
+    ls.operation_id AS id,
+    ls.block_num,
+    ov.trx_in_block,
+    encode(htv.trx_hash, 'hex') AS trx_hash,
+    ov.op_pos,
+    ls.op_type_id,
+    ov.body,
+    hot.is_virtual,
+    ov.timestamp,
+    NOW() - ov.timestamp AS age
+  FROM (
+  WITH op_filter AS MATERIALIZED (
+      SELECT ARRAY_AGG(ot.id) as op_id FROM hive.operation_types ot WHERE (CASE WHEN _filter IS NOT NULL THEN ot.id = ANY(_filter) ELSE TRUE END)
+  )
+    SELECT aov.operation_id, aov.op_type_id, aov.block_num
+    FROM hive.account_operations_view aov
+    WHERE aov.account_id = __account_id
+    AND (__no_ops_filter OR aov.op_type_id = ANY(ARRAY[(SELECT of.op_id FROM op_filter of)]))
+    ORDER BY aov.account_op_seq_no DESC
+    LIMIT _limit
+  ) ls
+  JOIN hive.operations_view ov ON ov.id = ls.operation_id
+  JOIN hive.operation_types hot ON hot.id = ls.op_type_id
+  LEFT JOIN hive.transactions_view htv ON htv.block_num = ls.block_num AND htv.trx_in_block = ov.trx_in_block
+  )
+
+-- filter too long operation bodies 
+  SELECT filtered_operations.id, filtered_operations.block_num, filtered_operations.trx_in_block, filtered_operations.trx_hash, filtered_operations.op_pos, filtered_operations.op_type_id, (filtered_operations.composite).body, filtered_operations.is_virtual, filtered_operations.timestamp, filtered_operations.age, (filtered_operations.composite).is_modified
+  FROM (
+  SELECT hafbe_backend.operation_body_filter(ov.body, ov.id, _body_limit) as composite, ov.id, ov.block_num, ov.trx_in_block, ov.trx_hash, ov.op_pos, ov.op_type_id, ov.is_virtual, ov.timestamp, ov.age
+  FROM operation_range ov 
+  ) filtered_operations
+  ORDER BY filtered_operations.id DESC;
+
+END
+$$;
+
+
 RESET ROLE;
