327663545aea156401323838e06e650f60097928
Michal Zander <mzander@syncad.com>
2024-05-16 08:29:42 +0000

    Unify blocksearch index

--- backend/hafbe_blocksearch.sql
+++ backend/hafbe_blocksearch.sql
@@ -1,78 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS btree_gin;
 
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_comment_author_permlink ON hive.operations USING gin
+-- Used in hafbe_backend.get_block_by_single_op (endpoint hafbe_endpoints.get_block_by_op)
+CREATE INDEX IF NOT EXISTS hive_operations_block_search_permlink_author ON hive.operations USING gin
 (
     jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
     jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_permlink')
+    block_num
 )
-WHERE op_type_id = 1;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_vote_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 0;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_delete_comment_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = any(ARRAY[17, 73]);
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_comment_options_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 19;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_author_reward_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 51;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_comment_benefactor_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 63;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_comment_payout_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 61;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_comment_reward_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 53;
-
--- Used in hafbe_backend.get_block_by_single_op and hafbe_backend.get_block_by_ops_group_by_block_num (endpoint hafbe_endpoints.get_block_by_op)
-CREATE INDEX IF NOT EXISTS hive_operations_effective_vote_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
-)
-WHERE op_type_id = 72;
+WHERE op_type_id = any(ARRAY[0, 1, 17, 19, 51, 52, 53, 61, 63, 72, 73]);
 
 -- Used in hafbe_backend.get_comment_operations and hafbe_backend.get_comment_operations_count (endpoint hafbe_endpoints.get_comment_operations)
 CREATE INDEX IF NOT EXISTS hive_operations_comment_search_permlink_author ON hive.operations USING btree
