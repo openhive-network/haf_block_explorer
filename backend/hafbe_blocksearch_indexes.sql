26111b82b4997c01951363e712087047140cadd6
Michal Zander <mzander@syncad.com>
2024-10-30 12:40:42 +0000

    Adjust names of block-search indexes

--- backend/hafbe_blocksearch_indexes.sql
+++ backend/hafbe_blocksearch_indexes.sql
@@ -9,29 +9,11 @@
 
 CREATE EXTENSION IF NOT EXISTS btree_gin;
 
--- Used in hafbe_backend.get_block_by_single_op (endpoint hafbe_endpoints.get_block_by_op)
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
-    END IF;
-  END
-$$;
-CREATE INDEX IF NOT EXISTS hive_operations_comment_author_permlink ON hive.operations USING gin
-(
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_author'),
-    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_permlink')
-)
-WHERE hive.operation_id_to_type_id(id) = 1;
-
-DO $$
-  BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_vote_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_vote_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_vote_author_permlink;
     END IF;
   END
 $$;
@@ -46,7 +28,24 @@ DO $$
   BEGIN
     IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
       RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+      DROP INDEX hive.hive_operations_comment_author_permlink;
+    END IF;
+  END
+$$;
+CREATE INDEX IF NOT EXISTS hive_operations_comment_author_permlink ON hive.operations USING gin
+(
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'parent_permlink')
+)
+WHERE hive.operation_id_to_type_id(id) = 1;
+
+DO $$
+  BEGIN
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_delete_comment_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_delete_comment_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_delete_comment_author_permlink;
     END IF;
   END
 $$;
@@ -59,9 +58,9 @@ WHERE hive.operation_id_to_type_id(id) = any(ARRAY[17, 73]);
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_options_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_comment_options_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_comment_options_author_permlink;
     END IF;
   END
 $$;
@@ -74,9 +73,9 @@ WHERE hive.operation_id_to_type_id(id) = 19;
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_author_reward_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_author_reward_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_author_reward_author_permlink;
     END IF;
   END
 $$;
@@ -89,9 +88,24 @@ WHERE hive.operation_id_to_type_id(id) = 51;
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_curation_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_curation_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_curation_author_permlink;
+    END IF;
+  END
+$$;
+CREATE INDEX IF NOT EXISTS hive_operations_curation_author_permlink ON hive.operations USING gin
+(
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'author'),
+    jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
+)
+WHERE hive.operation_id_to_type_id(id) = 52;
+
+DO $$
+  BEGIN
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_benefactor_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_comment_benefactor_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_comment_benefactor_author_permlink;
     END IF;
   END
 $$;
@@ -104,9 +118,9 @@ WHERE hive.operation_id_to_type_id(id) = 63;
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_payout_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_comment_payout_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_comment_payout_author_permlink;
     END IF;
   END
 $$;
@@ -119,9 +133,9 @@ WHERE hive.operation_id_to_type_id(id) = 61;
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_reward_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_comment_reward_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_comment_reward_author_permlink;
     END IF;
   END
 $$;
@@ -134,9 +148,9 @@ WHERE hive.operation_id_to_type_id(id) = 53;
 
 DO $$
   BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
+    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_effective_vote_author_permlink')) THEN
+      RAISE NOTICE 'Dropping invalid index hive_operations_effective_vote_author_permlink, it will be recreated';
+      DROP INDEX hive.hive_operations_effective_vote_author_permlink;
     END IF;
   END
 $$;
@@ -146,22 +160,6 @@ CREATE INDEX IF NOT EXISTS hive_operations_effective_vote_author_permlink ON hiv
     jsonb_extract_path_text(body_binary::jsonb, 'value', 'permlink')
 )
 WHERE hive.operation_id_to_type_id(id) = 72;
-
--- Used in hafbe_backend.get_comment_operations and hafbe_backend.get_comment_operations_count (endpoint hafbe_endpoints.get_comment_operations)
-DO $$
-  BEGIN
-    IF EXISTS(SELECT 1 FROM pg_index WHERE NOT indisvalid AND indexrelid = (SELECT oid FROM pg_class WHERE relname = 'hive_operations_comment_author_permlink')) THEN
-      RAISE NOTICE 'Dropping invalid index hive_operations_comment_author_permlink, it will be recreated';
-      DROP INDEX hive_operations_comment_author_permlink;
-    END IF;
-  END
-$$;
-CREATE INDEX IF NOT EXISTS hive_operations_comment_search_permlink_author ON hive.operations USING btree
-(
-    (body_binary::jsonb -> 'value' ->> 'author'),
-    (body_binary::jsonb -> 'value' ->> 'permlink')
-)
-WHERE hive.operation_id_to_type_id(id) = any(ARRAY[0, 1, 17, 19, 51, 52, 53, 61, 63, 72, 73]);
 
 ANALYZE VERBOSE hive.operations;
 ----------------------------------------------------------------------------------
@@ -173,6 +171,7 @@ ANALYZE VERBOSE hive.operations;
 17	"hive::protocol::delete_comment_operation"
 19	"hive::protocol::comment_options_operation"
 51	"hive::protocol::author_reward_operation"
+52	"hive::protocol::curation_reward_operation"
 53	"hive::protocol::comment_reward_operation"
 61	"hive::protocol::comment_payout_update_operation"
 63	"hive::protocol::comment_benefactor_reward_operation"
