4980e91e920ca22b533367111bcf93bb758927d1
Michal Zander <mzander@syncad.com>
2025-01-24 17:25:24 +0000

    Remove $$ from haf index registration

--- database/create_haf_indexes.sql
+++ database/create_haf_indexes.sql
@@ -3,7 +3,5 @@
 -- used by get_input_type (search for block_num by block's hash)
 SELECT hive.register_index_dependency(
     'hafbe_app',
-    $$
-    CREATE UNIQUE INDEX IF NOT EXISTS uq_blocks_hash ON hafd.blocks USING btree (hash);
-    $$
+    'CREATE UNIQUE INDEX IF NOT EXISTS uq_blocks_hash ON hafd.blocks USING btree (hash)'
 );
