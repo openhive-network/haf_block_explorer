cc6be18ea515cc446644dab755386ac2049c61d9
Michal Zander <mzander@syncad.com>
2025-02-04 12:13:00 +0000

    Set statement_timeout to 10s

--- backend/hafbe_roles.sql
+++ backend/hafbe_roles.sql
@@ -25,3 +25,5 @@ GRANT hafbe_user TO hafbe_owner;
 -- the current version of sqlfluff doesn't understand 'GRANT MAINTAIN'
 GRANT MAINTAIN ON ALL TABLES IN SCHEMA hafbe_app TO hived_group; -- noqa: PRS
 GRANT ALL ON SCHEMA hafbe_app TO hived_group;
+
+ALTER ROLE hafbe_user SET statement_timeout = '10s';
